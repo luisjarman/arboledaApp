5779caf4cfe5cbe63957dd0619e31eac2f9a42c6
Lewis <lujarman@gmail.com>
2017-11-25 18:50:30 +0100

    importing template 0.15

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('@AppBundle:Default:index.html.twig');
+        return $this->render('@App/default/index.html.twig');
     }
 }
