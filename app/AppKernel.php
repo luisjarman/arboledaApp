19fcfd5df4c17263d579e643314a280c0a15fd81
Lewis <lujarman@gmail.com>
2018-02-17 15:06:24 +0100

    Revert: test login? (1ec22ff)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
-            new KnpU\GuardBundle\KnpUGuardBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
